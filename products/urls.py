f29a755c21cdc6e2702776879ecfa77e10377447
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-01-05 16:09:55 +0000

    add urls for the product pages and rename offers to category

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import OffersView
+from .views import Categories, ProductDetail, Reviews
 
 urlpatterns = [
-    path('offers/', OffersView.as_view(), name='offers'),
+    path('category/<str:category>/', Categories.as_view(), name='category'),
+    path('product_details/<int:product_id>/', ProductDetail.as_view(), name='product_details'),
+    path('post_review/<int:product_id>/', Reviews.as_view(), name='post_review'),
 ]

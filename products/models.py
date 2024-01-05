f24a858c13c3a10391f523e87e7af610cac86eee
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-01-05 15:50:36 +0000

    Feat: add product reviews section  and upate the product model

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+ 
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
@@ -13,6 +14,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
+        
 
 class Brand(models.Model):
     name = models.CharField(max_length=254)
@@ -34,8 +36,10 @@ class Product(models.Model):
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
-                                 blank=True)
+    rating = models.IntegerField(
+        null=True,
+        blank=True
+    )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     brand = models.ForeignKey('Brand', null=True, blank=True,
@@ -44,7 +48,24 @@ class Product(models.Model):
     num_reviews = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
+    discounted_price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
     countInStock = models.IntegerField(null=True, blank=True, default=0)
-    
+    free_shipping = models.BooleanField(default=False, help_text='Is shipping free?')
+    discount_percentage = models.CharField(max_length=254, null=True, blank=True)
+
+
     def __str__(self):
         return self.name
+    
+    
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.rating)

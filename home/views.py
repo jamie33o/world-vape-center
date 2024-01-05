280c3522f2866d2ef8e35e93785d6388c3b247d7
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-01-05 01:57:09 +0000

    Chore: add url and view for homepage

--- home/views.py
+++ home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class IndexView(View):
+    """
+    View class for displaying a list of channels.
+    """
+
+    template_name = 'home/index.html'
+
+    def get(self, request):
+        """
+        Handle GET requests to display the list of channels.
+
+        Returns:
+            HttpResponse: Rendered template with channel information.
+        """
+        return render(request, self.template_name)

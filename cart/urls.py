736d0e78ba663b0add181cd481cee3aaee2ab00a
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-01-17 15:05:12 +0000

    Chore: change url path so it shows in breadcrums

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import cart_add, cart_delete, cart_summary, cart_update
 
 urlpatterns = [
-    path('', cart_summary, name='cart-summary'),
+    path('cart-summary/', cart_summary, name='cart-summary'),
     path('add/', cart_add, name='cart-add'),
     path('delete/', cart_delete, name='cart-delete'),
     path('update/', cart_update, name='cart-update'),

fa9399c8dd30ee7d73f91e8719b0e247223af112
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-01-25 00:17:00 +0000

    Fix: remove the prefix from url in allowed urls

--- world_vape_center/settings.py
+++ world_vape_center/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://world-vape-center-468f3f7d12a1.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'world-vape-center-468f3f7d12a1.herokuapp.com']
 
 # Application definition
 

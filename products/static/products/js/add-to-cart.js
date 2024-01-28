64631bdade59872a1d8c3f88315e2785525ece13
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-01-28 16:06:39 +0000

    Update: js for adding item to cart

--- products/static/products/js/add-to-cart.js
+++ products/static/products/js/add-to-cart.js
@@ -2,14 +2,23 @@ $(document).ready(function(){
         
     $(document).on("click", ".add-to-cart", function (e) {
         e.preventDefault();
+        let chosenOption = $('.options input:checked').val();
+        let csrf = $(this).data('csrf')
+        let url = $(this).data('url')
+
+        let qty = $("#select" + $(e.target).val() + " option:selected").text() 
+        if(!qty)
+            qty = 1
         $.ajax({
             type: "POST",
-            url: $(e.target).data('url'),
+            url: url,
+            headers: {
+                'X-CSRFToken': csrf
+            },
             data: {
                 product_id: $(e.target).val(),
-                product_quantity: "1",
-                csrfmiddlewaretoken: $(e.target).data('csrf'),
-                action: "post",
+                product_quantity: qty,
+                product_choice: chosenOption ? chosenOption : null,
             },
     
             success: function (json) {
@@ -17,7 +26,9 @@ $(document).ready(function(){
                 $(".cart-total").text('€ '+ json.cart_total);
             },
     
-            error: function (xhr, errmsg, err) {},
+            error: function (xhr, errmsg, err) {
+                console.log(err)
+            },
         });
     });
 })

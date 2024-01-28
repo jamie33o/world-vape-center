4c7ae2468d83af562736d0062203e55b183474df
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-01-28 16:07:19 +0000

    Add: checkout modals to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem, ShippingAddress
 
 # Register your models here.
+
+admin.site.register(Order)
+admin.site.register(OrderLineItem)
+admin.site.register(ShippingAddress)

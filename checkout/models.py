2104fed51bacb3c4c71cf36193232b8b8eed5922
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-01-28 15:47:21 +0000

    Add: Implement checkout models shipping address, order and orderlineitem

--- checkout/models.py
+++ checkout/models.py
@@ -1,103 +1,47 @@
 from django.db import models
-
 from django.contrib.auth.models import User
-
 from products.models import Product
 
 
 class ShippingAddress(models.Model):
-
-    full_name = models.CharField(max_length=300)
-
-    email = models.EmailField(max_length=255)
-
-    address1 = models.CharField(max_length=300)
-
-    address2 = models.CharField(max_length=300)
-
-    city = models.CharField(max_length=255)
-
-
-    # Optional
-
-    state = models.CharField(max_length=255, null=True, blank=True)
-
-    zipcode = models.CharField(max_length=255, null=True, blank=True)
-
-
-    # FK
-
-    # Authenticated / not authenticated users (bear in mind)
-
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    eircode = models.CharField(max_length=20, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-
-
 
     class Meta:
 
         verbose_name_plural = 'Shipping Address'
-
-
 
     def __str__(self):
 
         return 'Shipping Address - ' + str(self.id)
 
 
-
 class Order(models.Model):
-
-    full_name = models.CharField(max_length=300)
-
-    email = models.EmailField(max_length=255)
-
-    shipping_address = models.TextField(max_length=10000)
-
-
-    amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
-
-
-
-
-    date_ordered = models.DateTimeField(auto_now_add=True)
-
-
-    # FK
-
+    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=32, unique=True, null=False, editable=False)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
 
-
-    def __str__(self):
-
-        return 'Order - #' + str(self.id)
-
-
-
-
-class OrderItem(models.Model):
-
-    # FK -> 
-
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
-
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
-
-
-    quantity = models.PositiveBigIntegerField(default=1)
-
-    price = models.DecimalField(max_digits=8, decimal_places=2)    
-
-
-    # FK
-
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_option = models.CharField(max_length=2, null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False, default=1)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
 
     def __str__(self):
-
-        return 'Order Item - #' + str(self.id)
-
-
-
-        
+        return f'SKU {self.product.sku} on order {self.order.order_number}'

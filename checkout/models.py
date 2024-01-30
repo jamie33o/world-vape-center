7df222706938f0f327c223ea227edb33f97c82a4
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-01-30 14:13:56 +0000

    Fix: product option max lenght was 2 set to 100

--- checkout/models.py
+++ checkout/models.py
@@ -37,7 +37,7 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_option = models.CharField(max_length=2, null=True, blank=True)
+    product_option = models.CharField(max_length=100, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=1)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)

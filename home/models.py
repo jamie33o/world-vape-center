7ea3126162151e1e08c0c4109809352e950598a3
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-02-09 16:22:17 +0000

    Refactor: removed the home model for jumotron and overlay text

--- home/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class HomePage(models.Model):
-    title = models.CharField(max_length=100)
-    jumbotron_text = models.TextField()
-    jumbotron_image = models.ImageField(upload_to='jumbotron_images/')
-
-    def __str__(self):
-        return self.title
--- home/views.py
+++ home/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views import View
 from django.db.models import Avg
 from products.models import Product
-from .models import HomePage
 from products.models import Category
 
 
@@ -23,7 +22,6 @@ class IndexView(View):
             HttpResponse: Rendered template with channel information.
         """
         
-        homepage_instance = HomePage.objects.first()
         top_rated_products = Product.objects.annotate(avg_rating=Avg('review__rating')).filter(avg_rating__gte=3)
         if len(top_rated_products) < 10:
             categories = Category.objects.all()
@@ -39,9 +37,7 @@ class IndexView(View):
                 # Add the products to the selected list
                 top_rated_products.extend(products_in_category)
 
-
         context = {
-            'homepage_instance': homepage_instance,
             'top_rated_products': top_rated_products,
         }
         return render(request, self.template_name, context)

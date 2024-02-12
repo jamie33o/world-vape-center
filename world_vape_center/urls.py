f4c09ac755292fa188872c7f23c67411e23ec108
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-02-12 01:52:14 +0000

    Add: urls for site map

--- world_vape_center/urls.py
+++ world_vape_center/urls.py
@@ -17,6 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from products.sitemaps import ProductSitemap, CategorySitemap
+from .sitemaps import StaticViewSitemap
+
+
+sitemaps = {
+    'products': ProductSitemap,
+    "static": StaticViewSitemap,
+    'catgories': CategorySitemap
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +37,10 @@ urlpatterns = [
     path('newsletter/', include('newsletter.urls')),
     path('help/', include('help.urls')),
     path('checkout/', include('checkout.urls')),
+    path("sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="sitemap",
+    )
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

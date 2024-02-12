be3e3d2d4e1e5309afa53aa7558b17c2d8a302fb
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-02-12 02:06:23 +0000

    Refactor: change what is displayed in urls

--- products/urls.py
+++ products/urls.py
@@ -3,9 +3,9 @@ from .views import ProductDetailView, ReviewsView, CategoryView
 
 urlpatterns = [
     path('', CategoryView.as_view(), name='categories'),
-    path('category/<str:category>/', CategoryView.as_view(), name='category'),
-    path('product_details/<str:slug>/', ProductDetailView.as_view(), name='product_details'),
-    path('post_review/<int:product_id>/', ReviewsView.as_view(), name='post_review'),
-    path('delete_review/<int:review_id>/', ReviewsView.as_view(), name='delete_review'),
-    path('filter/<str:category>', CategoryView.as_view(), name='filter'),
+    path('<str:category>/', CategoryView.as_view(), name='category'),
+    path('<str:category>/<str:slug>/', ProductDetailView.as_view(), name='product_details'),
+    path('post_review/<int:product_id>', ReviewsView.as_view(), name='post_review'),
+    path('delete_review/<int:review_id>', ReviewsView.as_view(), name='delete_review'),
+    path('<str:category>', CategoryView.as_view(), name='filter'),
 ]

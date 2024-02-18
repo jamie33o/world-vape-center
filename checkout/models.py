600f477646710ef2c6f24d1ca61706ab6e42c3be
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-02-18 17:35:53 +0000

    Add discount field to order model

--- checkout/models.py
+++ checkout/models.py
@@ -21,6 +21,7 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     sub_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    discount = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, related_name='user_orders')

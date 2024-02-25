0d023544af7d449e5a66c8081d5390ff2b42ea12
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-02-25 15:05:13 +0000

    Refactor: join views for subscribe and unsub in one view and remove the other and urls for it

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,8 +1,9 @@
+"""
+url path for the newsletter app
+"""
 from django.urls import path
-from .views import subscribe_newsletter, un_subscribe_newsletter
+from .views import subscribe_newsletter
 
 urlpatterns = [
     path('subscribe/', subscribe_newsletter, name='subscribe_newsletter'),
-    path('un_subscribe/', un_subscribe_newsletter, name='un_subscribe_newsletter'),
-
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,31 +1,60 @@
+"""
+Subscribe to Newsletter
+
+Handles the subscription to the newsletter. Accepts POST
+requests with a valid email address.
+Returns a JSON response indicating success or failure.
+
+Parameters:
+    request (HttpRequest): The incoming HTTP request.
+
+Returns:
+    JsonResponse: A JSON response indicating the success
+    or failure of the subscription.
+"""
 from django.http import JsonResponse
 from django.core.mail import send_mail
 from django.conf import settings
-from django.core.exceptions import ValidationError
 from django.template.loader import render_to_string
+from django.views.decorators.http import require_POST
 from .models import Subscriber
 from .forms import NewsletterSubscriptionForm
 
 
+@require_POST
 def subscribe_newsletter(request):
-    response_data = {'success': False, 'message': ''}
+    """
+    Unsubscribe from Newsletter
 
-    if request.method == 'POST':
-        form = NewsletterSubscriptionForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
+    Handles the unsubscription from the newsletter.
+    Accepts POST requests with a valid email address.
+    Returns a JSON response indicating success or failure.
 
-            try:
-                # Create a new Subscriber instance
-                subscriber = Subscriber(email=email)
-                subscriber.full_clean()  # Validate the model fields (including uniqueness)
-                subscriber.save()
+    Parameters:
+        request (HttpRequest): The incoming HTTP request.
 
-                context = {'email': subscriber.email}
-                email_content = render_to_string('sub_thank_you_email.html', context)
+    Returns:
+        JsonResponse: A JSON response indicating the
+        success or failure of the unsubscription.
+    """
 
-                email_subject = 'Thank you for Subscribing'
-                recipient_list = [subscriber.email]
+    form = NewsletterSubscriptionForm(request.POST)
+    if form.is_valid():
+        email = form.cleaned_data['email']
+            
+        try:
+            # Create a new Subscriber instance
+            subscriber = Subscriber.objects.filter(email=email).first()
+            if subscriber:
+                # The email exists, remove the subscriber
+                email = subscriber.email
+                context = {'email': email}
+
+                subscriber.delete()
+                email_content = render_to_string('unsub_email.html', context)
+
+                email_subject = 'You have Unsubscribed'
+                recipient_list = [email]
                 from_email = settings.EMAIL_HOST_USER
 
                 send_mail(
@@ -37,67 +66,50 @@ def subscribe_newsletter(request):
                     fail_silently=False
                 )
 
-                response_data['success'] = True
-                response_data['message'] = 'Successfully subscribed to the newsletter.'
-            except ValidationError as e:
-                error_message = e.message_dict.get('email', [''])[0]
+                response_data = {
+                    'status': 'success',
+                    'message': 'Successfully un-subscribed from the newsletter.'
+                }
 
-                response_data['message'] = error_message
-        else:
-            response_data['message'] = 'Invalid email address.'
+                return JsonResponse(response_data)
+
+            # Create a new Subscriber instance
+            subscriber = Subscriber(email=email)
+            subscriber.full_clean()  # Validate the model fields (including uniqueness)
+            subscriber.save()
+
+            context = {'email': subscriber.email,}
+            email_content = render_to_string('sub_thank_you_email.html', context)
+
+            email_subject = 'Thank you for Subscribing'
+            recipient_list = [subscriber.email]
+            from_email = settings.EMAIL_HOST_USER
+
+            send_mail(
+                email_subject,
+                '',
+                from_email,
+                recipient_list,
+                html_message=email_content,
+                fail_silently=False
+            )
+            response_data = {
+                'status': 'success',
+                'message': 'Successfully subscribed to the newsletter.'
+            }
+            return JsonResponse(response_data)
+
+        except Exception as e:
+            print(e)
+            response_data = {
+                    'status': 'danger',
+                    'message': e
+                }
+            return JsonResponse(response_data)
+
     else:
-        response_data['message'] = 'Invalid request method.'
-
-    return JsonResponse(response_data)
-
-
-def un_subscribe_newsletter(request):
-    response_data = {'success': False, 'message': ''}
-
-    if request.method == 'POST':
-        form = NewsletterSubscriptionForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-
-            try:
-                # Create a new Subscriber instance
-                subscriber = Subscriber.objects.filter(email=email).first()
-                if subscriber:
-                    # The email exists, remove the subscriber
-                    email = subscriber.email
-                    subscriber.delete()
-
-                    context = {'email': email}
-                    email_content = render_to_string('unsub_email.html', context)
-
-                    email_subject = 'You have Unsubscribed'
-                    recipient_list = [email]
-                    from_email = settings.EMAIL_HOST_USER
-
-                    send_mail(
-                    email_subject,
-                        '',
-                        from_email,
-                        recipient_list,
-                        html_message=email_content,
-                        fail_silently=False
-                    )
-
-                    response_data['success'] = True
-                    response_data['message'] = 'Successfully un-subscribed from the newsletter.'
-                
-                else:
-                    response_data['message'] = f'No email matching {email}'
-
-            except ValidationError as e:
-                error_message = e.message_dict.get('email', [''])[0]
-
-                response_data['message'] = error_message
-        else:
-            response_data['message'] = 'Invalid email address.'
-    else:
-        response_data['message'] = 'Invalid request method.'
-
-    return JsonResponse(response_data)
-
-
+        response_data = {
+                    'status': 'danger',
+                    'message': form.errors
+                }
+        return JsonResponse(response_data)

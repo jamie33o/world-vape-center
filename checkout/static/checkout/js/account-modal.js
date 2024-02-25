2e6590f6c5b35efcf51751452fbb18e6e180d5fa
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-02-25 15:11:58 +0000

    Docs: add comments

--- checkout/static/checkout/js/account-modal.js
+++ checkout/static/checkout/js/account-modal.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
+    // Select the element with class 'account-modal-title'
     const modelTitle = $('.account-modal-title')
-    $('.toggle-form').click(function(){
-        $('.form').each(function(){
 
+    // Attach a click event handler to elements with class 'toggle-form'
+    $('.toggle-form').click(function(){
+        // Toggle the visibility of all elements with class 'form'
+        $('.form').each(function(){
             $(this).toggleClass('d-none');
         });
+
+        // Change the text content of the element with class 'account-modal-title'
+        // based on its current text content
         if (modelTitle.text() ===  'Sign In') {
             modelTitle.text('Sign Up');
         } else {

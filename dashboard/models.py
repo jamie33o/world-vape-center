da41e934d5b5c439639b7ebbe3fb0b3941abbffa
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-03-25 02:32:12 +0000

    add modal for ticket and ticket message responses

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Ticket(models.Model):
+
+    TITLE_CHOICES = [
+        ('question', 'Question'),
+        ('site_error', 'Site Error'),
+        ('order_issue', 'Order Issue'),
+        ('delivery_issue', 'Delivery Issue'),
+        ('refund_request', 'Refund Request'),
+        ('account_issue', 'Account Issue'),
+        ('feedback', 'Feedback'),
+        ('other', 'Other'),
+    ]
+
+    title = models.CharField(max_length=20, choices=TITLE_CHOICES)
+    description = models.TextField()
+    status = models.CharField(max_length=20, default='Open')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return dict(self.TITLE_CHOICES)[self.title]
+
+
+class Ticket_messages(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='ticket_messages')
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender} on {self.created_at}"

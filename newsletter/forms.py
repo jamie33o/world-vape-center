6322bd57083367cd2946e89d62e4ca700113f4fe
jamie33o <oneilljamie24@gmail.com>
2025-03-08 22:08:42 +0000

    Enhance newsletter subscription form; update email input widget for improved styling and user experience

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -8,6 +8,13 @@ class NewsletterSubscriptionForm(forms.Form):
     addresses for newsletter subscriptions.
     """
 
-    email = forms.EmailField(label='Email:',
-                             widget=forms.TextInput(attrs={'id':
-                                                           'email_newsletter'}))
+    email = forms.EmailField(
+        label="Email:",
+        widget=forms.EmailInput(
+            attrs={
+                "id": "email_newsletter",
+                "class": "w-full text-black border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500",
+                "placeholder": "Enter your email",
+            }
+        ),
+    )
